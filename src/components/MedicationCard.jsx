529508acd30fbbbcf683335620b23fdb0bae40ea
Nichole Axel <nfrancis22@gmail.com>
2022-06-09 21:34:16 -0400

    scrolling cards

--- src/components/MedicationCard.jsx
+++ src/components/MedicationCard.jsx
@@ -2,21 +2,21 @@ import React, { useState, useEffect } from "react";
 
 export default function MedicationCard({ medication }) {
   return (
-    <div class="card-row">
-      <div class="card-body">
-        <p class="card-header">{`${medication?.name}`}</p>
-        <p class="card-uses">Common Uses: {`${medication?.uses}`}</p>
-        <p class="card-category">
-          Medication Category: {`${medication?.category}`}
-        </p>
-        <p class="card-contra">
-          Contraindications: {`${medication?.contraindication}`}
-        </p>
-        <p class="species">
-          🐶 :{medication?.canine ? "✅" : "❎"} 🐱 :
-          {medication?.feline ? "✅" : "❎"}
-        </p>
-      </div>
+    // <div class="card-row">
+    <div class="card-body">
+      <p class="card-header">{`${medication?.name}`}</p>
+      <p class="card-uses">Common Uses: {`${medication?.uses}`}</p>
+      <p class="card-category">
+        Medication Category: {`${medication?.category}`}
+      </p>
+      <p class="card-contra">
+        Contraindications: {`${medication?.contraindication}`}
+      </p>
+      <p class="card-species">
+        🐶 :{medication?.canine ? "✅" : "❎"} 🐱 :
+        {medication?.feline ? "✅" : "❎"}
+      </p>
     </div>
+    // </div>
   );
 }
